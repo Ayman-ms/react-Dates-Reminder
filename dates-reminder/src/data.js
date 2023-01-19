4fb34efce4923ada1f19e2b54cbb159ea52a549e
Mohamad Ayman Mslmani <104221357+Ayman-ms@users.noreply.github.com>
2023-01-19 09:36:52 +0100

    Update data.js

--- dates-reminder/src/data.js
+++ dates-reminder/src/data.js
@@ -1,8 +1,6 @@
 export const person = [{ id: '0', name: 'Dr. Alex', date: '5 PM', img: 'p1.jpg' },
 { id: '1', name: 'Work Presentation', date: '4 PM', img: 'p1.jpg' },
-{ id: '0', name: 'Dr. Alex', date: '5 PM', img: 'p1.jpg' },
-{ id: '1', name: 'Work Presentation', date: '4 PM', img: 'p1.jpg' },
-{ id: '0', name: 'Dr. Alex', date: '5 PM', img: 'p1.jpg' },
-{ id: '1', name: 'Work Presentation', date: '4 PM', img: 'p1.jpg' },
-{ id: '0', name: 'Dr. Alex', date: '5 PM', img: 'p1.jpg' },
-{ id: '1', name: 'Work Presentation', date: '4 PM', img: 'p1.jpg' },]
+{ id: '2', name: 'Dr. Alex', date: '5 PM', img: 'p1.jpg' },
+{ id: '3', name: 'Work Presentation', date: '4 PM', img: 'p1.jpg' },
+{ id: '4', name: 'Dr. Alex', date: '5 PM', img: 'p1.jpg' },
+]
